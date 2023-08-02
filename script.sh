c6d840a0d65c22cb4843f4e7384036bb25ae84de
manitheja Yamavaram <109070761+ManithejaCyberark@users.noreply.github.com>
2023-08-02 21:20:31 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -14,12 +14,10 @@ JWT_AUDIENCE="jenkins-server"
 #build dockerfile
 docker build -t jenkins:jcasc .
 
-#docker run
-# --env CONJUR_ACCESS_TOKEN=$ACCESS_TOKEN \
 docker run --name jenkins_jcasc --rm -p 8085:8080 \
 --env JENKINS_ADMIN_ID=admin \
 --env JENKINS_ADMIN_PASSWORD=password \
---env CLOUD_API_KEY="cqet201bf5g8a1g8pt331tmpv643ma4e5p350z3hy124ksg02q34pc" \
+--env CLOUD_API_KEY=$CLOUD_API_KEY \
 --env JENKINS_URL=${JENKINS_URL} \
 --env CONJURACCOUNT=${CONJURACCOUNT} \
 --env APPLIANCEURL=${APPLIANCEURL} \
