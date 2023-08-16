e0307def9d1c92d839b31a19e675afcb2bfd9a08
manitheja Yamavaram <109070761+ManithejaCyberark@users.noreply.github.com>
2023-08-16 11:47:30 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -12,18 +12,29 @@
 # CONJUR_CREDENTIALS_PLUGIN=conjur-credentials:1.0.15 
 
 #build dockerfile
+
 docker build -t jenkins:jcasc .
 
-docker images ls | grep jenkins:jcasc
+docker images ls
 
+# docker run --name jenkins_jcasc --rm -p 8085:8080 \
+# --env JENKINS_ADMIN_ID=admin \
+# --env JENKINS_ADMIN_PASSWORD=password \
+# --env CLOUD_API_KEY=$CLOUD_API_KEY \
+# --env JENKINS_URL=${JENKINS_URL} \
+# --env CONJURACCOUNT=${CONJURACCOUNT} \
+# --env APPLIANCEURL=${APPLIANCEURL} \
+# --env SERVICEID=${SERVICEID}  \
+# --env JWT_AUDIENCE=${JWT_AUDIENCE} \
+# # --env CONJUR_CREDENTIALS_PLUGIN=${CONJUR_CREDENTIALS_PLUGIN} \
+# jenkins:jcasc
 docker run --name jenkins_jcasc --rm -p 8085:8080 \
 --env JENKINS_ADMIN_ID=admin \
 --env JENKINS_ADMIN_PASSWORD=password \
---env CLOUD_API_KEY=$CLOUD_API_KEY \
+--env CLOUD_API_KEY=${CLOUD_API_KEY} \
 --env JENKINS_URL=${JENKINS_URL} \
 --env CONJURACCOUNT=${CONJURACCOUNT} \
 --env APPLIANCEURL=${APPLIANCEURL} \
 --env SERVICEID=${SERVICEID}  \
 --env JWT_AUDIENCE=${JWT_AUDIENCE} \
-# --env CONJUR_CREDENTIALS_PLUGIN=${CONJUR_CREDENTIALS_PLUGIN} \
-jenkins:jcasc
+  jenkins:jcasc
